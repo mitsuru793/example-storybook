8fb2cf41a019bf539476d825d9700dc4a9a67b22
mitsuru793 <mitsuru793@gmail.com>
2020-03-20 03:41:43 +0900

    finish chapter 7 Addons
    skip chapter 6 Testing but read it

--- taskbox/src/components/Task.js
+++ taskbox/src/components/Task.js
@@ -11,17 +11,23 @@ export default function Task({task: {id, title, state}, onArchiveTask, onPinTask
                     disabled={true}
                     name="checked"
                 />
-                <span className="checkbox-custom" onClick={() => onArchiveTask(id)} />
+                <span className="checkbox-custom" onClick={() => onArchiveTask(id)}/>
             </label>
             <div className="title">
-                <input type="text" value={title} readOnly={true} placeholder="Input title" />
+                <input
+                    type="text"
+                    value={title}
+                    readOnly={true}
+                    placeholder="Input title"
+                    style={{textOverflow: 'ellipsis'}}
+                />
             </div>
 
             <div className="actions" onClick={event => event.stopPropagation()}>
                 {state !== 'TASK_ARCHIVED' && (
                     // eslint-disable-next-line jsx-a11y/anchor-is-valid
                     <a onClick={() => onPinTask(id)}>
-                        <span className={`icon-star`} />
+                        <span className={`icon-star`}/>
                     </a>
                 )}
             </div>
